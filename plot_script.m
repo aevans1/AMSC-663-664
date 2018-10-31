82817f13e05e29ce18190da2be4f8dc69fa302c5
Luke Evans <luke.evans49@gmail.com>
2018-10-30 23:18:33 -0400

    moved plot script one directory up

--- plot_script.m
+++
@@ -1,9 +0,0 @@
-x = linspace(0,1,257);
-y = linspace(0,1,257);
-[X,Y]=meshgrid(x,y);
-U = load('U.txt');
-V = U;
-%%
-%load in different U from other git branch
-U = load('U.txt');
-%contourf(X,Y,U,20);
